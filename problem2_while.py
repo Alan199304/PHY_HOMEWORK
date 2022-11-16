22d05675da995e9c1fc4d0f3b60ef7a458c94629
Your Name <you@example.com>
2022-11-16 19:37:01 +0800

    while loop answer

---
+++ problem2_while.py
@@ -0,0 +1,13 @@
+temperature = 90
+
+while temperature>75:
+    print("Temperature is",temperature, "- crank the AC!")
+    temperature = temperature -3
+print(temperature,"Ahh, that's is better")
+
+#    Temperature is 90 — crank the AC!
+ #   Temperature is 87 — crank the AC!
+  #  Temperature is 84 — crank the AC!
+   # Temperature is 81 — crank the AC!
+ #   Temperature is 78 — crank the AC!
+  #  75. Ahh, that's better.
