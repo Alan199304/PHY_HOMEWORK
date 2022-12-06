db96344f45bd0c0b6fb2014ee9efaa77d4a56532
Your Name <you@example.com>
2022-12-06 22:54:00 +0800

    set tuples homework

---
+++ set_tuple2.py
@@ -0,0 +1,9 @@
+employee1 = {"name":"Ron Swanson","age":55,"department":"Management","phone":'555-1234',"salary":6000}
+employee2 = {"name":"Leslie Knope","age":50,"department":"Middle Management","phone":'555-4321',"salary":6500}
+employee3 = {"name":"Andy Dwyer","age":40,"department":" Shoe Shining","phone":'555-1122',"salary":4000}
+employee4 = {"name":"April Ludgate","age":30,"department":" Administration","phone":'555-3345',"salary":4500}
+
+print(employee1["name"],"in",employee1["department"],"can be reached at",employee1["phone"],".")
+print(employee2["name"],"in",employee2["department"],"can be reached at",employee2["phone"],'.')
+print(employee3["name"],"in",employee3["department"],"can be reached at",employee3["phone"],'.')
+print(employee4["name"],"in",employee4["department"],"can be reached at",employee4["phone"],'.')
